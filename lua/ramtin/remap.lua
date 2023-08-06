07980120898afedae069ed06a5b99d26fc8b4e08
Ramtin <Ramtin.abadi@gmail.com>
2023-08-06 13:13:48 +0530

    Remapped enter and backspace in the normal mode

--- lua/ramtin/remap.lua
+++ lua/ramtin/remap.lua
@@ -9,5 +9,11 @@ vim.keymap.set("i", "<C-b>", '<left>')
 vim.keymap.set("i", "<C-p>", '<up>')
 vim.keymap.set("i", "<C-n>", '<down>')
 
+-- Remapping Enter to add new line
+vim.api.nvim_set_keymap('n', '<CR>', 'o<Esc>', { noremap = true, silent = true })
+
+-- Remapping Backspace to remove a line
+vim.api.nvim_set_keymap('n', '<BS>', 'ddk', { noremap = true, silent = true })
+
 -- visual multi
 
